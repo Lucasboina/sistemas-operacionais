f75e287fc02c55a9c5ed32c418e55162b6b3d6da
Lucasboina <lucasmmantelli@gmail.com>
2024-10-10 14:06:51 -0300

    Exercicio 2

--- Aula1/exercicio2.c
+++ Aula1/exercicio2.c
@@ -3,13 +3,25 @@
 #include <stdlib.h>  //uso de ponteiros e alocação dinâmica
 #include <unistd.h>  //acesso ao POSIX do Sistema Operacional
 
-int mat[3][7];
+long mat[3][7];
+int somaTotal = 0;
+pthread_mutex_t mutexsum;
 
 void *countSum(void *tid)
 {
     int somaLinha = 0;
-    // deve fazer a soma da linha da matriz
-    // esperar todas as treads finalizaresm soma
+    int i;
+    long *linha;
+    linha = (long *)tid;
+    for (i = 0; i < 7; i++)
+    {
+        somaLinha += linha[i];
+    }
+    pthread_mutex_lock(&mutexsum);
+    somaTotal += somaLinha;
+    pthread_mutex_unlock(&mutexsum);
+
+    pthread_exit(NULL);
 }
 
 int main()
@@ -17,7 +29,7 @@ int main()
     pthread_t t1, t2, t3; // criando t1 e t2 do tipo thread
     int create;           // variável que recebe o retorno da função pthread_create()
     int i, j;
-
+    srand(time(NULL));
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 7; j++)
@@ -26,12 +38,18 @@ int main()
         }
     }
 
-    printf("Main: criando a thread %ld\n", 1);
-    create = pthread_create(&t1, NULL, countSum, (void *)1); // criando thread 1
-    printf("Main: criando a thread %ld\n", 2);
-    create = pthread_create(&t2, NULL, countSum, (void *)2); // criando thread 2
-    printf("Main: criando a thread %ld\n", 3);
-    create = pthread_create(&t2, NULL, countSum, (void *)3); // criando thread 3
+    printf("Main: criando a thread %d\n", 1);
+    pthread_create(&t1, NULL, countSum, (void *)mat[0]); // criando thread 1
+    printf("Main: criando a thread %d\n", 2);
+    create = pthread_create(&t2, NULL, countSum, (void *)mat[1]); // criando thread 2
+    printf("Main: criando a thread %d\n", 3);
+    create = pthread_create(&t2, NULL, countSum, (void *)mat[2]); // criando thread 3
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
+
     printf("Main: finalizando\n");
+    printf("%d", somaTotal);
+    pthread_mutex_destroy(&mutexsum);
     return 0;
 }
